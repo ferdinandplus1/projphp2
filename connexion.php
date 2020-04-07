5315d854456a1e76f157fd2b7a2b5e2c4cafde96
july floraa <floratatchou@gmail.com>
2020-04-07 15:59:21 +0200

    nouveau formulaire

--- connexion.php
+++ connexion.php
@@ -1,8 +1,12 @@
 <?php 
+
+
 //session_start();
 $_SESSION['pseudonyme'] = $_POST['pseudonyme'];
+// connexiion ala base de données
 
-try{
+
+    try{
 
     $bdd = new PDO('mysql:host=localhost;dbname=todo','root','');
     }
@@ -10,26 +14,55 @@ try{
     {
     die ('Erreur: ' .$e->getMessage());
     }
-     $checkbox=$_POST['sexe'];
-     $chk="";
-     foreach($checkbox as $chk1)
-     {
-        $chk .=$chk1. ",";
+    //
 
-     }
+    echo "j'ai passe cette etape1";
+    if (isset($_POST['email'])) { //Test du mail
 
+        $_POST['email'] = htmlspecialchars($_POST['email']);
+        if (preg_match("#^[a-z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$#", $_POST['email'])) 
+        { //format normal d'un mail
+            echo "j'ai passe cette etape";
+        } else {
+            $erreur = "Adresse mail avec un mauvais format (ex correct: truc_bidule@site.fr) !";
+        }
+    }
+
+    if (isset($_POST['pseudonyme']))
+    {
+        $_POST['pseudonyme'] = htmlspecialchars($_POST['pseudonyme']);
+        if (preg_match("#^[a-zA-Z](.[^<>?]){6-20}#", $_POST['pseudonyme']))
+        {
+           $erreur = NULL; 
+        } else{
+            $erreur = 'Vous avez mal ecrit votre pseudonyme';
+        }
+    }
     $req = $bdd-> prepare("INSERT INTO connexion (sexe, pseudonyme, email, motDePasse)
-    VALUES ('$chk', ?, ?, ?)") ;
+    VALUES (?, ?, ?, ?)") ;
     $req->execute(array(
-    $_POST['sexe'],
+    $_POST['choix'],
     $_POST['pseudonyme'],
     $_POST['email'],
     $_POST['motDePasse'],
    
     ));
+
+
+    echo "kjykjdgjkghghjfghfgfgfljmj";
+    // afficher le message de bienvenue  si Homme ou Femme
+    if ($_POST['choix']== "Homme")
+    {
+        echo 'Bienvnue Monsieur '.$_SESSION['pseudonyme'].'<br/>';
+    }else{
+        echo 'Bienvenue Madame '.$_SESSION['pseudonyme'].'<br/>';
+    }
     
-    echo  'Bienvenue Madame '.$_SESSION['pseudonyme'].'<br/>' .' vous avez bien ete ajoute';
+
     
+    
+
+
     
     
     //$reponse=$bdd->query('SELECT * FROM connexion');
--- formulaire.php
+++ formulaire.php
@@ -1,8 +1,3 @@
-<?php 
-
-
-
-?>
 
 
 
@@ -12,49 +7,38 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css" media="screen" type="text/css"/>
     <title>SITE WEB</title>
+
 </head>
 <body>
-<form action="connexion.php" method="post">
-<p>
-    choississez votre sexe :<br/>
-    <label>Homme</label>
-    <input type="checkbox" name="sexe []" value="Homme"  id="Homme" ><br/>
-    <label>Femme</label>
-    <input type="checkbox" name="sexe []" value="Femme" id="Femme" ><br/>
-</p>
-<p>
-    <label> pseudo (15 caracteres au maximum):</label>
-    <input type="text" name="pseudonyme" id="pseudonyme"><br/>
-    <?php
-    if (isset($_POST['pseudonyme']))
-    {
-        $_POST['pseudonyme'] = htmlspecialchars($_post['pseudonyme']);
-        if (preg_match("#^[a-zA-Z](.[^<>?]){6-20}#", $_POST['pseudonyme']))
-        {
-           $erreur = NULL; 
-        } else{
-            $erreur = 'Vous avez mal ecrit votre pseudonyme';
-        }
-    }
+    <div id="container">
+<form  method="post"  action="connexion.php">
+    <h1>Connexion</h1>
+    <p>
+    <label for="sexe" >sexe</label>
+        <select name="choix" id="">
+            <option value="Homme">Homme</option>
+            <option value="Femme">Femme</option>
+        </select>
 
-     ?>
-</p>
-<p>
-    <label>email:</label>
-    <input type="text" name="email" id="email"><br/> 
-</p>
+    </p>
 
+    <strong> PSEUDO</strong> <em> (15 caracteres au maximum)</em> <br/>
+    <input type="text" name="pseudonyme" placeholder="Entrer votre Pseudo" id="pseudonyme"><br/>
+    <strong>EMAIL</strong><br/>
+    <input type="text" name="email" placeholder="Entrer votre Email" id="email"><br/>
+    
+
+    <strong>PASSWORD </strong><br/>
+    <em>seuls les mots en lettres et chiffres sont autorisés</em><br/>
+    <input type="password" name="motDePasse" placeholder="Entrer votre Password" id="password"><br/>
+    
+     
 <p>
-    <label >mot de passe:</label>
-    <input type="password" name="password" id="password"><br/><em>seuls les mots en lettres et chiffres sont autorisés</em>
-</p>
-<p>
-    <input type="submit" value="envoyer"><br/>
+    <input type="submit" id="submit" value="Se connecter"><br/>
 </p>
 </form>
     
 </body>
-</html><p>
-    <input type="submit" value="envoyer"><br/>
-</p>
+</html
